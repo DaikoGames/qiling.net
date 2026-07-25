d1aebb7647e1f4ec872afe562d4fb3be02ed56e8
DaikoGames <136380205+DaikoGames@users.noreply.github.com>
2026-07-25 13:01:27 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using CliWrap;
+
+namespace Qiling.net
+{
+    public class Qiling
+    {
+        public async void Emulate(string OStoEmulate, string CPUtypeEmulatedOS, string PathToExecutable)
+        {
+            string PythonPath = "";
+            if(OperatingSystem.IsWindows())
+            {
+                if(RuntimeInformation.OSArchitecture == Architecture.X86)
+                {
+                    PythonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "./Windows/Python_x86.exe");
+                }
+                if(RuntimeInformation.OSArchitecture == Architecture.X64)
+                {
+                    PythonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "./Windows/Python_x64.exe");
+                }
+                if(RuntimeInformation.OSArchitecture == Architecture.Arm64)
+                {
+                    PythonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "./Windows/Python_arm64.exe");
+                }
+            }
+
+            if(OperatingSystem.IsLinux())
+            {
+                
+                if(RuntimeInformation.OSArchitecture == Architecture.X64)
+                {
+                    PythonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "./Linux/Python_x64");
+                }
+                if(RuntimeInformation.OSArchitecture == Architecture.Arm64)
+                {
+                    PythonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "./Linux/Python_arm64");
+                }
+            }
+
+            if(OperatingSystem.IsMacOS())
+            {
+                if(RuntimeInformation.OSArchitecture == Architecture.X64)
+                {
+                    PythonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "./MacOS/Python_x64.exe");
+                }
+                if(RuntimeInformation.OSArchitecture == Architecture.Arm64)
+                {
+                    PythonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "./MacOS/Python_arm64.exe");
+                }
+            }
+
+            if(OStoEmulate == "Windows" | OStoEmulate == "windows")
+            {
+                
+            }
+
+            if(OStoEmulate == "Linux" | OStoEmulate == "linux")
+            {
+                
+            }
+
+            if(OStoEmulate == "MacOS" | OStoEmulate == "macos")
+            {
+                
+            }
+        }
+    }
+}
